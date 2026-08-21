84bb00fde94c99afa9b3af9847195a652b4b73c6
Drishti-Sharma-Cse <drishtii.sh08@gmail.com>
2026-08-21 20:42:05 +0530

    Added Positive negative Java Program

---
+++ PositiveNegative.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PositiveNegative {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number");
+        int n = sc.nextInt(); 
+        if (n>=0){
+            System.out.println("Number is positive");
+        }else{
+            System.out.println("Number is negative");
+        }
+        
+    sc.close();
+   }
+}
